4f44fa966d293bd6527dc3274c4e29dc82450751
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 22:32:53 +0800

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad2;
+
+/**
+ *
+ * @author renzj
+ */
+public class Rectangle extends Parallelogram {
+    @Override
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 4 right angles");
+    }
+}
