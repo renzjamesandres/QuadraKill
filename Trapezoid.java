b712527cfe33219a34d59b597e3da2b324d56691
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 22:34:14 +0800

    Create Trapezoid.java

---
+++ Trapezoid.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad2;
+
+/**
+ *
+ * @author renzj
+ */
+public class Trapezoid extends Quadrilateral {
+    @Override
+    public void showDescription() {
+        System.out.println("- has 1 pair of parallel sides");
+        super.showDescription();
+    }
+}
