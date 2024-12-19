17c495c5dc9977c753fb68a079dc0dd49c00c15b
renzjamesandres <renzjamesandres68@gmail.com>
2024-12-19 22:33:31 +0800

    Create Rhombus.java

---
+++ Rhombus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package runquad2;
+
+/**
+ *
+ * @author renzj
+ */
+public class Rhombus extends Parallelogram {
+    @Override
+    public void showDescription() {
+        System.out.println("- has 4 congruent sides");
+        super.showDescription();
+    }
